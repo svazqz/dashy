337fc326ce256a0e1392428613c35abdce4bda73
Sergio Vazquez <sergio@incotechsystems.com>
2025-03-26 13:56:41 -0600

    Calculate opacity for bg

--- app/controllers/api/Config.php
+++ app/controllers/api/Config.php
@@ -5,11 +5,16 @@ namespace Controllers\API;
 use Core;
 
 class Config extends Core\APIController {
+  private function calculateOpacity() {
+    $hour = (int)date('G');
+    return ($hour >= 6 && $hour <= 21) ? 0 : 0.7;
+  }
+
   public function show() {
     header('Content-Type: application/json');
     echo json_encode(array(
         "backgroundImage" => "bg3.jpg",
-        "overlayOpacity" => 0
+        "overlayOpacity" => $this->calculateOpacity()
     ));
   }
 }
