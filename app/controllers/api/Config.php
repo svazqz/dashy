3b3b7ae469f81f8ebf3721dfa8d6e07d8df0a765
Sergio Vazquez <sergio@incotechsystems.com>
2025-03-26 16:23:34 -0600

    Add hour prop to data

--- app/controllers/api/Config.php
+++ app/controllers/api/Config.php
@@ -7,14 +7,15 @@ use Core;
 class Config extends Core\APIController {
   private function calculateOpacity() {
     $hour = (int)date('G');
-    return ($hour >= 6 && $hour <= 21) ? 0 : 0.7;
+    return (6 <= $hour && $hour <= 21) ? 0 : 0.7;
   }
 
   public function show() {
     header('Content-Type: application/json');
     echo json_encode(array(
         "backgroundImage" => "bg3.jpg",
-        "overlayOpacity" => $this->calculateOpacity()
+        "overlayOpacity" => $this->calculateOpacity(),
+        "localHour" => (int)date('G')
     ));
   }
 }
